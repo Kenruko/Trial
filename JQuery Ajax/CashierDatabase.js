e4f405738dc734c10b097e9ef08d1367bae63f3d
Kenruko <estrada.johnchrysler.123@gmail.com>
2022-02-04 22:58:00 +0800

    LAGOT AKO
    HHEHEHEH

---
+++ JQuery Ajax/CashierDatabase.js
@@ -0,0 +1,117 @@
+$(document).ready(function() {
+    $(window).on("load", () => {
+        $.ajax({
+            url: 'http://Localhost:8080/Cashier/Orders',
+            method: 'GET',
+            dataType: 'html'
+        }).done((data) => {
+            if (data == undefined) {
+                alert('ERROR!!!');
+            }
+            $('#orders').empty();
+            $('#orders').append(data);
+        });
+    });
+    $('#placeOrder').click(() => {
+        const orderKey = $('#orderKey').val();
+        const orderVal = $('#orderVal').val();
+        $.ajax({
+            url: 'http://Localhost:8080/Cashier/Add-Order',
+            method: 'POST',
+            data: {key: orderKey, quantity: orderVal},
+            dataType: 'text'
+        }).done((data) => {
+            if (data == 'Incorrect') {
+                alert('Product is NOT FOUND!!!');
+            }
+            else if (data == 'Order-Added') {
+                $.ajax({
+                    url: 'http://Localhost:8080/Cashier/Orders',
+                    method: 'GET',
+                    dataType: 'html'
+                }).done((data) => {
+                    if (data == undefined) {
+                        alert('ERROR!!!');
+                    }
+                    $('.Order input').val('');
+                    $('#orders').empty();
+                    $('#orders').append(data);
+                });
+            }
+            else {
+                alert('ERROR!!!');
+            }
+        });
+    });
+    $(document).on('click', '.delete', function() {
+        const name = $(this).closest('tr').find('.name').html();
+        // console.log(name);
+        $.ajax({
+            url: 'http://Localhost:8080/Cashier/Delete',
+            method: 'POST',
+            data: {key: name},
+            dataType: 'text'
+        }).done((data) => {
+            if (data == 'Order-Deleted') {
+                $.ajax({
+                    url: 'http://Localhost:8080/Cashier/Orders',
+                    method: 'GET',
+                    dataType: 'html'
+                }).done((data) => {
+                    if (data == undefined) {
+                        alert('ERROR!!!');
+                        return;
+                    }
+                    $('#orders').empty();
+                    $('#orders').append(data);
+                });
+            }
+            else {
+                alert('ERROR!!!!');
+            }
+        });
+    });
+    $('#resetOrders').click(() => {
+        const total = $('#Total').val();
+        $.ajax({
+            url: 'http://Localhost:8080/Cashier/Reset-Orders',
+            method: 'POST',
+            data: { price: total },
+            dataType: 'text'
+        }).done((data) => {
+            if (data == 'Reset-Success') {
+                alert('Orders is now Reset');
+            }
+            else {
+                alert('ERROR!!');
+                return;
+            }
+        });
+        $.ajax({
+            url: 'http://Localhost:8080/Cashier/Orders',
+            method: 'GET',
+            dataType: 'html'
+        }).done((data) => {
+            if (data == undefined) {
+                alert('ERROR!!!');
+                return;
+            }
+            $('#orders').empty();
+            $('#orders').append(data);
+        });
+        $.ajax({
+            url: 'http://Localhost:8080/Display',
+            method: 'GET',
+            dataType: 'html'
+        }).done((data) => {
+            if (data == undefined) {
+                alert('ERROR!!!!!');
+                return;
+            }
+            // console.log(data);
+            $('#Products').empty();
+            $('#Products').append(data);
+        });
+    });
+});
+
